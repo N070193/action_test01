eb5b8314d78d0e744aaf55040be6d44a1a294015
N070193 <82585651+N070193@users.noreply.github.com>
2023-04-09 10:30:41 +0530

    Update test.py

--- TEST/test.py
+++ TEST/test.py
@@ -1,5 +1,6 @@
 import argparse
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
 parser = argparse.ArgumentParser(description="main")
 parser.add_argument('-p', '--path', help="Path",action="store", required=True)
@@ -10,12 +11,33 @@ args = parser.parse_args()
 # deployment_file= args.deployment_file
 GOOGLE_CLOUD_STORAGE_CREDENTIALS = args.path
 
-client=bigquery.Client.from_service_account_json(GOOGLE_CLOUD_STORAGE_CREDENTIALS)
-dataset_id='bigquery-demo-377718.dataset_github'
-dataset=bigquery.Dataset(dataset_id)
-dataset.location="US"
-dataset.description="dataset from python"
+credentials = service_account.Credentials.from_service_account_file(GOOGLE_CLOUD_STORAGE_CREDENTIALS)
 
-dataset_ref=client.create_dataset(dataset,timeout=30)
+project_id = 'bigquery-demo-377718'
+client = bigquery.Client(credentials= credentials,project=project_id)
 
-print("created dataset {}.{}".format(client.project, dataset_ref.dataset_id))
+query_job = client.query("""
+   SELECT *
+   FROM dataset_py.table_py
+   LIMIT 1000 """)
+
+results = query_job.result() # Wait for the job to complete.
+
+# parser = argparse.ArgumentParser(description="main")
+# parser.add_argument('-p', '--path', help="Path",action="store", required=True)
+
+
+# # parse the arguments
+# args = parser.parse_args()
+# # deployment_file= args.deployment_file
+# GOOGLE_CLOUD_STORAGE_CREDENTIALS = args.path
+
+# client=bigquery.Client.from_service_account_json(GOOGLE_CLOUD_STORAGE_CREDENTIALS)
+# dataset_id='bigquery-demo-377718.dataset_github'
+# dataset=bigquery.Dataset(dataset_id)
+# dataset.location="US"
+# dataset.description="dataset from python"
+
+# dataset_ref=client.create_dataset(dataset,timeout=30)
+
+# print("created dataset {}.{}".format(client.project, dataset_ref.dataset_id))
