152cd36deb12782a13d7c632758486be13d5b670
N070193 <82585651+N070193@users.noreply.github.com>
2023-04-09 09:55:31 +0530

    Create test.py

---
+++ TEST/test.py
@@ -0,0 +1,20 @@
+from google.cloud import bigquery
+
+parser = argparse.ArgumentParser(description="main")
+parser.add_argument('-p', '--path', help="Path",action="store", required=True)
+
+
+# parse the arguments
+args = parser.parse_args()
+# deployment_file= args.deployment_file
+GOOGLE_CLOUD_STORAGE_CREDENTIALS = args.path
+
+client=bigquery.Client.from_service_account_json(GOOGLE_CLOUD_STORAGE_CREDENTIALS)
+dataset_id='bigquery-demo-377718.dataset_github'
+dataset=bigquery.Dataset(dataset_id)
+dataset.location="US"
+dataset.description="dataset from python"
+
+dataset_ref=client.create_dataset(dataset,timeout=30)
+
+print("created dataset {}.{}".format(client.project, dataset_ref.dataset_id))
